653e94ba7a56f826d2249aef603da89f7b240667
cj.park <sisyphus9402@gmail.com>
2023-06-20 17:44:01 +0900

    test: Add a test code for a <table> tag in Jinja2 template

--- web_app/app/views/main_view.py
+++ web_app/app/views/main_view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 from werkzeug.utils import redirect
 from app.engine.case_manager import CaseManager
 
@@ -7,16 +7,31 @@ bp = Blueprint("main", __name__, url_prefix="/")
 @bp.route("/")
 def index():
     artifacts = [
-        "Chrome",
-        "Edge",
+        # "Chrome",
+        # "Edge",
         # "RecycleBin",
         # "Prefetch",
         # "JumpList",
         # "LogonEvent",
-        # "USB(EventLog)",
+        "USB(EventLog)",
         # "WLAN",
     ]
     cm = CaseManager(_artifacts=artifacts)
     cm.parse_all()
 
-    return render_template("page/home/index.html", cm=cm)
+
+    return render_template("page/home/index.html", forensic_artifact=cm.forensic_artifact)
+    # return redirect(url_for("main.data"))  # test code
+
+
+
+
+# # test code
+# @bp.route('/data')
+# def data():
+#     data = [
+#         {"name": "Alice", "email": "alice@example.com"},
+#         {"name": "Bob", "email": "bob@example.com"},
+#         {"name": "Charlie", "email": "charlie@example.com"}
+#     ]
+#     return render_template("page/home/index.html", data=data)
