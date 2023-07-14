9516d0f184de6c5962dfb70ba36ab30dd5d36077
cj.park <sisyphus9402@gmail.com>
2023-07-14 16:09:46 +0900

    feat: Complete a demo version.

--- web_app/app/views/services/digital_forensics.py
+++ web_app/app/views/services/digital_forensics.py
@@ -14,7 +14,7 @@ def process():
         # "Prefetch",
         # "JumpList",
         # "LogonEvent",
-        "USB(EventLog)",
+        # "USB(EventLog)",
         # "WLAN",
     ]
     cm = CaseManager(_artifacts=artifacts)
@@ -31,53 +31,66 @@ def process():
     return redirect(url_for("dashboard.digital_forensics"))
 
 
-@bp.route("/Internet", methods=["GET"])
+@bp.route("/internet", methods=["GET"])
 def internet():
-    title = "Statistics"
-    return render_template(
-        "page/dashboard/table_internet.jinja-html",
-        title=title
-    )
-
-@bp.route("/logon_event", methods=["GET"])
-def logon_event():
-    title = "Logon History"
+    title = "인터넷 사용기록"
     """
         'records' variable is list of str(json), which is a result of 'json.dumps()'.
         So, you have to convert it to list of dict(json) using 'json.loads'.
     """
     records = [
         json.loads(record)
-        for record in session.get("logon_event", "{}")
+        # for record in session.get("logon_event", "{}")
+        for record in session.get("recyclebin", "{}")
     ]
 
     return render_template(
-        "page/dashboard/table_logon_event.jinja-html",
+        "page/services/digital_forensics/table_internet.jinja-html",
+        title = title,
+        records=records
+    )
+
+@bp.route("/logon_event", methods=["GET"])
+def logon_event():
+    title = "사용자 로그온 기록"
+    """
+        'records' variable is list of str(json), which is a result of 'json.dumps()'.
+        So, you have to convert it to list of dict(json) using 'json.loads'.
+    """
+    records = [
+        json.loads(record)
+        # for record in session.get("logon_event", "{}")
+        for record in session.get("recyclebin", "{}")
+    ]
+
+    return render_template(
+        "page/services/digital_forensics/table_logon_event.jinja-html",
         title = title,
         records=records
     )
 
 @bp.route("/jumplist", methods=["GET"])
 def jumplist():
-    title = "File History"
+    title = "파일 열람기록"
     """
         'records' variable is list of str(json), which is a result of 'json.dumps()'.
         So, you have to convert it to list of dict(json) using 'json.loads'.
     """
     records = [
         json.loads(record)
-        for record in session.get("jumplist", "{}")
+        # for record in session.get("jumplist", "{}")
+        for record in session.get("recyclebin", "{}")
     ]
 
     return render_template(
-        "page/dashboard/table_jumplist.jinja-html",
+        "page/services/digital_forensics/table_jumplist.jinja-html",
         title = title,
         records=records
     )
 
 @bp.route("/recyclebin", methods=["GET"])
 def recyclebin():
-    title = "Recyclebin History"
+    title = "휴지통 데이터 목록"
     """
         'records' variable is list of str(json), which is a result of 'json.dumps()'.
         So, you have to convert it to list of dict(json) using 'json.loads'.
@@ -95,14 +108,15 @@ def recyclebin():
 
 @bp.route("/usb_event", methods=["GET"])
 def usb_event():
-    title = "USB History"
+    title = "USB 연결 이벤트"
     """
         'records' variable is list of str(json), which is a result of 'json.dumps()'.
         So, you have to convert it to list of dict(json) using 'json.loads'.
     """
     records = [
         json.loads(record)
-        for record in session.get("usb_event", "{}")
+        # for record in session.get("usb_event", "{}")
+        for record in session.get("recyclebin", "{}")
     ]
 
     return render_template(
@@ -120,16 +134,15 @@ def prefetch():
     """
     records = [
         json.loads(record)
-        for record in session.get("prefetch", "{}")
+        for record in session.get("recyclebin", "{}")
+        # for record in session.get("prefetch", "{}")
     ]
 
     return render_template(
-        "page/dashboard/table_prefetch.jinja-html",
+        "page/services/digital_forensics/table_prefetch.jinja-html",
         title = title,
         records=records
     )
-
-
 
 @bp.route("/wlan_event", methods=["GET"])
 def wlan_event():
