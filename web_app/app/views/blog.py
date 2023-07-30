6c5e2d312c9710bdd1c16e13e84ea9da5fb9a1e3
cj.park <sisyphus9402@gmail.com>
2023-07-30 13:14:49 +0900

    design: Change a blog page design

--- web_app/app/views/blog.py
+++ web_app/app/views/blog.py
@@ -1,10 +1,39 @@
 import json
 import requests
-from flask import Blueprint, render_template
+import math
+
+from flask import Blueprint, render_template, request
+from bs4 import BeautifulSoup
 
 from config import tistory_api_token
 
 bp = Blueprint("blog", __name__, url_prefix="/blog")
+
+def pagination(records: list[dict], page: int, per_page: int):
+    """
+        This function returns a list of dict(json) data for the current page.
+        :param records: list of dict(json) data
+        :param page: current page number
+        :param per_page: number of items per page
+    """
+    BLOCK_SIZE = 5  # number of pages in the navigation block
+
+    # Pagination
+    start = (page - 1) * per_page  # first item to display on this page
+    end = start + per_page  # last item to display on this page
+    items_on_page = records[start:end]  # items to display on this page
+
+    total = len(records)  # total number of items in the list
+    last_page = math.ceil(total / per_page)  # last page number
+
+    block_num = int((page - 1) / BLOCK_SIZE)  # current block number
+    block_start = int((BLOCK_SIZE * block_num) + 1)  # first page number in the block (1, 6, 11, ...
+    block_end = math.ceil(block_start + (BLOCK_SIZE - 1))  # last page number in the block
+
+    if block_end > last_page:
+        block_end = last_page
+
+    return items_on_page, total, last_page, block_start, block_end
 
 @bp.route("/")
 def main():
@@ -42,15 +71,37 @@ def main():
             posts.append(
                 {
                     "id": item["id"],
-                    "postUrl": item["postUrl"],
                     "title": item["title"],
+                    "postUrl": item["postUrl"],
                     "date": item["date"],
                 }
             )
 
+    for post in posts:
+        post_url = f"https://www.tistory.com/apis/post/read?access_token={tistory_api_token}&output=json&blogName={blog_name}&postId={post['id']}"
+
+        post_response = json.loads(requests.get(post_url).text)
+        content = post_response["tistory"]["item"]["content"]
+
+        soup = BeautifulSoup(content, "html.parser")
+        img_tag = soup.find("img")
+        post["thumbnail"] = img_tag["src"]
+            
+    # Pagination variables
+    page = request.args.get('page', default=1, type=int)
+    per_page = request.args.get('per_page', default=6, type=int)
+
+    # Pagination
+    items_on_page, total, last_page, block_start, block_end = pagination(posts, page, per_page)
+
+
     return render_template(
         "page/blog/main.jinja-html",
-        posts=posts
+        posts=items_on_page,
+        total=total,
+        last_page=last_page,
+        block_start=block_start,
+        block_end=block_end,
     )
 
 @bp.route("/<int:post_id>")
